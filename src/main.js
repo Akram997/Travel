0dc84818ab005653f4ffa72052beb631370c3fb4
Akram997 <l303216115@qq.com>
2019-07-13 09:47:10 +0800

    项目初始化

--- src/main.js
+++ src/main.js
@@ -1,9 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
+// 解决手机端link链接延迟执行的问题  npm install fastclick --save 
+import FastClick from 'fastclick'
+// 引入router.js文件
 import router from './router/index.js'
+// 引入统一各个手机浏览器初始样式的不一致
+import './assets/styles/reset.css'
+// 解决各个分辨率手机的初始像素问题
+import './assets/styles/border.css'
 
 
 Vue.config.productionTip = false
+// 调用方法 就可解决延迟执行链接问题
+FastClick.attach(document.body)
 
 new Vue({
   render: h => h(App),
--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-      <h3>Home</h3>
+      hello world
   </div>
 </template>
 
